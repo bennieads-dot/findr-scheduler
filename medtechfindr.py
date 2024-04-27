be512b085056fc1307708812d6787831841944f7
bennieads-dot <benjamin.j.adams144@gmail.com>
2024-04-27 16:30:46 -0400

    initial medtechfindr

---
+++ medtechfindr.py
@@ -0,0 +1,62 @@
+import json
+import requests
+from prefect.variables import Variable
+from prefect.blocks.system import Secret
+from prefect import flow, task
+
+@task
+def pull_jobs(payload:dict):
+    try:
+      url = Variable.get('pull_jobs_func_url').value
+      func_key = Secret.load('pull-jobs-func-key').get()
+      headers = {
+        'x-functions-key': func_key,
+        'Content-Type': 'application/json'
+      }
+      response = requests.post(url, headers=headers, data=json.dumps(payload))
+      if response.status_code != 200:
+        raise Exception(f'Error in pull task: {response.status_code} - {response.text}')
+      print(f'successfully posted jobs for {payload.schema}')
+    except Exception as e:
+      raise e
+
+@task
+def post_jobs(payload:dict):
+    try:
+      url = Variable.get('post_jobs_func_url').value
+      func_key = Secret.load('post-jobs-func-key').get()
+      headers = {
+        'x-functions-key': func_key,
+        'Content-Type': 'application/json'
+      }
+      response = requests.post(url, headers=headers, data=json.dumps(payload))
+      if response.status_code != 200:
+        raise Exception(f'Error in post task: {response.status_code} - {response.text}')
+      print(f'successfully posted jobs for {payload.schema}')
+    except Exception as e:
+      raise e
+
+
+@flow(log_prints=True)
+def medtechfindr():
+    try:
+      pull_jobs({
+          "schema": "medtechfindr",
+          "query": "CT tech in Baltimore, MD",
+          "job_titles": "Radiologic Technologist, Radiographer, X-Ray Technician, Computed Tomography (CT) Technologist, Magnetic Resonance Imaging (MRI) Technologist, Mammographer, Nuclear Medicine Technologist, Radiation Therapist, Interventional Radiology Technologist, Vascular Interventional Technologist, Sonographer, Ultrasound Technician, Cardiovascular Technologist, Cardiac Catheterization Technologist, Echocardiography Technician, Electrocardiogram (EKG/ECG) Technician, Stress Test Technician, Cardiac Sonographer, Vascular Technologist, Cardiac Electrophysiology Specialist, Cardiac Device Technician, Pacemaker Technician, Perfusionist"
+      })
+
+      pull_jobs({
+          "schema": "medtechfindr",
+          "query": "CT tech in New York, Ny",
+          "job_titles": "Radiologic Technologist, Radiographer, X-Ray Technician, Computed Tomography (CT) Technologist, Magnetic Resonance Imaging (MRI) Technologist, Mammographer, Nuclear Medicine Technologist, Radiation Therapist, Interventional Radiology Technologist, Vascular Interventional Technologist, Sonographer, Ultrasound Technician, Cardiovascular Technologist, Cardiac Catheterization Technologist, Echocardiography Technician, Electrocardiogram (EKG/ECG) Technician, Stress Test Technician, Cardiac Sonographer, Vascular Technologist, Cardiac Electrophysiology Specialist, Cardiac Device Technician, Pacemaker Technician, Perfusionist"
+      })
+    except Exception as e:
+       print(e)
+
+if __name__ == "__main__":
+    medtechfindr.serve(
+        name="medtechfindr Daily",
+        cron = "0 1 * * *",
+        description="Pulls and posts medtechfindr jobs daily at",
+    )
